51afb1c6fb2df72d83e6fb45f7dd9b9f425156e9
frblondin <thomas@caudal.fr>
2018-11-29 22:16:42 +0100

    Fix AccessViolationException in Grep

--- src/GitObjectDb/Services/IObjectRepositorySearch.cs
+++ src/GitObjectDb/Services/IObjectRepositorySearch.cs
@@ -15,16 +15,18 @@ namespace GitObjectDb.Services
         /// </summary>
         /// <param name="repository">The repository.</param>
         /// <param name="content">The content.</param>
+        /// <param name="comparison">The type of comparison.</param>
         /// <returns>All objects containing the <paramref name="content"/> value.</returns>
-        IEnumerable<IModelObject> Grep(IObjectRepository repository, string content);
+        IList<IModelObject> Grep(IObjectRepository repository, string content, StringComparison comparison = StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
         /// Searches for the specified content in the repository.
         /// </summary>
         /// <param name="container">The container.</param>
         /// <param name="content">The content.</param>
+        /// <param name="comparison">The type of comparison.</param>
         /// <returns>All objects containing the <paramref name="content"/> value.</returns>
-        IEnumerable<IModelObject> Grep(IObjectRepositoryContainer container, string content);
+        IList<IModelObject> Grep(IObjectRepositoryContainer container, string content, StringComparison comparison = StringComparison.OrdinalIgnoreCase);
 
         /// <summary>
         /// Gets the objects having a reference to this instance.
@@ -32,7 +34,7 @@ namespace GitObjectDb.Services
         /// <typeparam name="TModel">The type of the model.</typeparam>
         /// <param name="node">The object.</param>
         /// <returns>All objects having a link whose target is the <paramref name="node"/>.</returns>
-        IEnumerable<IModelObject> GetReferrers<TModel>(TModel node)
+        IList<IModelObject> GetReferrers<TModel>(TModel node)
             where TModel : class, IModelObject;
     }
 }
