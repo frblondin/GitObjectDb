0198914d96d92f0e59ffef40f17fdf4b6d2e7539
frblondin <thomas@caudal.fr>
2022-11-14 14:54:37 +0100

    Fix misc minor bugs

---
+++ src/tests/GitObjectDb.SystemTextJson.Tests/GitObjectDbSystemTextJsonFixture.cs
@@ -0,0 +1,10 @@
+using GitObjectDb.Tests;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace GitObjectDb.SystemTextJson.Tests;
+
+[SetUpFixture]
+public class GitObjectDbSystemTextJsonFixture : GitObjectDbFixture
+{
+}
