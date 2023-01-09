53ab35e5d9112895ebf07d8a057f5c900806e1c8
frblondin <thomas@caudal.fr>
2023-01-09 17:42:05 +0100

    Fix typo

--- src/GitObjectDb.Api.OData/Tools/TypeExtensions.cs
+++ src/GitObjectDb.Api.OData/Tools/TypeExtensions.cs
@@ -10,7 +10,7 @@ internal static class TypeExtensions
     /// </summary>
     /// <param name="property">The property to be analyzed.</param>
     /// <param name="predicate">The predicate to be applied on generic arguments.</param>
-    /// <param name="type">The generic argument type marching the <paramref name="predicate"/>.</param>
+    /// <param name="type">The generic argument type matching the <paramref name="predicate"/>.</param>
     /// <returns><c>true</c> if a matching enumerable could be found, <c>false</c> otherwise.</returns>
     internal static bool IsEnumerable(this PropertyInfo property, Predicate<Type> predicate, out Type? type)
     {
