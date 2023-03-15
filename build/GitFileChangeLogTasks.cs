24cf11ea66883fd14f3b424e2df2e837d893f3b6
frblondin <thomas@caudal.fr>
2023-03-15 17:05:29 +0100

    Do not create NuGet packages when project files haven't been changed since previous tag [skip release notes]

---
+++ build/GitFileChangeLogTasks.cs
@@ -0,0 +1,26 @@
+﻿using Nuke.Common.Tools.Git;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal static class GitFileChangeLogTasks
+{
+    public static IEnumerable<string> ChangedFilesSinceLastTag()
+    {
+        var lastTag = GitTasks
+            .Git("describe --tags --abbrev=0")
+            .Select(x => x.Text)
+            .FirstOrDefault();
+        Serilog.Log.Information("Found most recent tag '{LastTag}'", lastTag);
+
+        var result = lastTag != null ? GitTasks
+            .Git($"diff --name-only {lastTag}..HEAD")
+            .Select(x => x.Text)
+            .ToList() : null;
+        Serilog.Log.Information("Found {ModifiedFilesCount} changes since last tag", result?.Count);
+
+        return result;
+    }
+}
