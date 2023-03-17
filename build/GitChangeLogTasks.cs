cd59dd45e705ca00dbdde65bd524a2514c301436
frblondin <thomas@caudal.fr>
2023-03-17 14:38:25 +0100

    Create GitHub release description using commit titles

--- build/GitChangeLogTasks.cs
+++ build/GitChangeLogTasks.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-internal static class GitFileChangeLogTasks
+internal static class GitChangeLogTasks
 {
     public static IEnumerable<string> ChangedFilesSinceLastTag()
     {
@@ -17,6 +17,23 @@ internal static class GitFileChangeLogTasks
 
         var result = lastTag != null ? GitTasks
             .Git($"diff --name-only {lastTag}..HEAD")
+            .Select(x => x.Text)
+            .ToList() : null;
+        Serilog.Log.Information("Found {ModifiedFilesCount} changes since last tag", result?.Count);
+
+        return result;
+    }
+
+    public static IEnumerable<string> CommitsSinceLastTag()
+    {
+        var lastTag = GitTasks
+            .Git("describe --tags --abbrev=0")
+            .Select(x => x.Text)
+            .FirstOrDefault();
+        Serilog.Log.Information("Found most recent tag '{LastTag}'", lastTag);
+
+        var result = lastTag != null ? GitTasks
+            .Git($"log --pretty=format:%s {lastTag}..HEAD")
             .Select(x => x.Text)
             .ToList() : null;
         Serilog.Log.Information("Found {ModifiedFilesCount} changes since last tag", result?.Count);
