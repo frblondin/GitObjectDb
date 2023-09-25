a8acfc88c05a0616ad6915bb212a5ff31719b8de
frblondin <thomas@caudal.fr>
2023-09-25 19:09:54 +0200

    Switch comparison library from CompareNETObjects to ObjectsComparer

--- src/GitObjectDb/Comparison/NodeComparerLimitedToPath.cs
+++ src/GitObjectDb/Comparison/NodeComparerLimitedToPath.cs
@@ -1,128 +1,29 @@
-using Fasterflect;
-using GitObjectDb.Tools;
-using KellermanSoftware.CompareNetObjects;
-using KellermanSoftware.CompareNetObjects.TypeComparers;
-using System;
-using System.Collections;
-using System.Collections.Concurrent;
+using ObjectsComparer;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 namespace GitObjectDb.Comparison;
-internal class NodeComparerLimitedToPath : ClassComparer
+internal class NodeComparerLimitedToPath : AbstractValueComparer, IEqualityComparer<Node>
 {
-    private static readonly ConcurrentDictionary<Type, IEnumerable<PropertyInfo>> _cache = new();
+    public static NodeComparerLimitedToPath Instance { get; } = new NodeComparerLimitedToPath();
 
-    public NodeComparerLimitedToPath(RootComparer rootComparer)
-        : base(rootComparer)
+    public override bool Compare(object obj1, object obj2, ComparisonSettings settings)
     {
+        if (obj1 == obj2)
+        {
+            return true;
+        }
+
+        return obj1 switch
+        {
+            Node node1 when obj2 is Node node2 => Equals(node1, node2),
+            IEnumerable<Node> nodeEnum1 when obj2 is IEnumerable<Node> nodeEnum2 => nodeEnum1.OrderBy(n => n.Path)
+                .SequenceEqual(nodeEnum2.OrderBy(n => n.Path), this),
+            _ => false,
+        };
     }
 
-    public override bool IsTypeMatch(Type? type1, Type? type2) =>
-        (type1 is not null && GetNodeReferences(type1).Any()) &&
-        (type2 is not null && GetNodeReferences(type2).Any());
+    public bool Equals(Node x, Node y) => x.Path?.Equals(y.Path) ?? false;
 
-    public override void CompareType(CompareParms parms)
-    {
-        base.CompareType(parms);
-
-        CompareReferencedNodePathChanges(parms);
-    }
-
-    private void CompareReferencedNodePathChanges(CompareParms parms)
-    {
-        try
-        {
-            parms.Result.AddParent(parms.Object1);
-            parms.Result.AddParent(parms.Object2);
-            if (parms.Object1 is IEnumerable || parms.Object1 != parms.Object2)
-            {
-                var type1 = parms.Object1.GetType();
-                var type2 = parms.Object2.GetType();
-                if (!ExcludeLogic.ShouldExcludeClass(parms.Config, type1, type2))
-                {
-                    parms.Object1Type = type1;
-                    parms.Object2Type = type2;
-                    PerformReferencedNodeCompareProperties(parms);
-                }
-            }
-        }
-        finally
-        {
-            parms.Result.RemoveParent(parms.Object1);
-            parms.Result.RemoveParent(parms.Object2);
-        }
-    }
-
-    private void PerformReferencedNodeCompareProperties(CompareParms parms)
-    {
-        var currentProperties1 = GetNodeReferences(parms.Object1Type);
-        var currentProperties2 = GetNodeReferences(parms.Object2Type);
-        foreach (var property in currentProperties1)
-        {
-            CompareReferencedNodeProperty(parms, property, currentProperties2);
-            if (parms.Result.ExceededDifferences)
-            {
-                break;
-            }
-        }
-    }
-
-    private void CompareReferencedNodeProperty(CompareParms parms, PropertyInfo info, IEnumerable<PropertyInfo> object2Properties)
-    {
-        if (!info.CanRead || (!parms.Config.CompareReadOnly && !info.CanWrite))
-        {
-            return;
-        }
-        var secondObjectInfo = GetSecondObjectInfo(info, object2Properties);
-        if ((parms.Config.IgnoreObjectTypes || parms.Config.IgnoreConcreteTypes) &&
-            secondObjectInfo is null &&
-            parms.Config.IgnoreMissingProperties)
-        {
-            return;
-        }
-        if (info.PropertyType.IsNodeEnumerable(out var _))
-        {
-            var values1 = (IEnumerable<Node>?)Reflect.PropertyGetter(info).Invoke(parms.Object1);
-            var values2 = (IEnumerable<Node>?)(secondObjectInfo is not null ? Reflect.PropertyGetter(secondObjectInfo).Invoke(parms.Object2) : null);
-            RootComparer.Compare(new CompareParms
-            {
-                Result = parms.Result,
-                Config = parms.Config,
-                ParentObject1 = parms.Object1,
-                ParentObject2 = parms.Object2,
-                Object1 = values1?.Select(n => n.Path),
-                Object2 = values2?.Select(n => n.Path),
-                BreadCrumb = AddBreadCrumb(parms.Config, parms.BreadCrumb, info.Name),
-                Object1DeclaredType = typeof(IEnumerable<DataPath>),
-                Object2DeclaredType = typeof(IEnumerable<DataPath>),
-            });
-        }
-        else
-        {
-            var node1 = (Node?)Reflect.PropertyGetter(info).Invoke(parms.Object1);
-            var node2 = (Node?)(secondObjectInfo is not null ? Reflect.PropertyGetter(secondObjectInfo).Invoke(parms.Object2) : null);
-            RootComparer.Compare(new CompareParms
-            {
-                Result = parms.Result,
-                Config = parms.Config,
-                ParentObject1 = parms.Object1,
-                ParentObject2 = parms.Object2,
-                Object1 = node1?.Path,
-                Object2 = node2?.Path,
-                BreadCrumb = AddBreadCrumb(parms.Config, parms.BreadCrumb, info.Name),
-                Object1DeclaredType = typeof(DataPath),
-                Object2DeclaredType = typeof(DataPath),
-            });
-        }
-    }
-
-    private static PropertyInfo? GetSecondObjectInfo(PropertyInfo info, IEnumerable<PropertyInfo> object2Properties) =>
-        object2Properties.FirstOrDefault(p => p.Name == info.Name);
-
-    private static IEnumerable<PropertyInfo> GetNodeReferences(Type type) => _cache.GetOrAdd(type, static t =>
-        (from p in t.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-         where p.PropertyType.IsNode() || p.PropertyType.IsNodeEnumerable(out var _)
-         select p).ToList());
+    public int GetHashCode(Node obj) => obj?.Path?.GetHashCode() ?? 0;
 }
